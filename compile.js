9f05ec37147f6866a03ed68db2dbd6874a2de1b3
rayGreycloud <rayGreycloud@gmail.com>
2018-01-27 07:27:06 -0800

    Refactor compile call into separate variable

--- compile.js
+++ compile.js
@@ -5,4 +5,8 @@ const solc = require('solc');
 const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
 const source = fs.readFileSync(lotteryPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':Lottery'];
+const compiled = solc.compile(source, 1);
+
+module.exports = compiled.contracts[':Lottery'];
+
+// module.exports = solc.compile(source, 1).contracts[':Lottery'];
