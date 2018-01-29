51760fe277b34ad933580d7b456e65fb4c1d48b5
rayGreycloud <rayGreycloud@gmail.com>
2018-01-29 11:14:53 -0800

    Change pickWinner to set lastWinner instead of returning it; Add method to return last winner

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.17;
 contract Lottery {
   address public manager;
   address[] public players;
-  address public winner;
+  address public lastWinner;
   
   function Lottery() public {
     manager = msg.sender;
@@ -19,14 +19,12 @@ contract Lottery {
     return uint(keccak256(block.difficulty,now, players));
   }
   
-  function pickWinner() public restricted returns (address) {
+  function pickWinner() public restricted {
     require(players.length > 0);
     uint index = random() % players.length;
-    winner = players[index];    
+    lastWinner = players[index];    
     players[index].transfer(this.balance);
     players = new address[](0);
-    
-    return winner;
   }
   
   modifier restricted() {
@@ -37,5 +35,8 @@ contract Lottery {
   function getPlayers() public view returns (address[]) {
     return players;
   }
-
+  
+  function getLastWinner() public view returns (address) {
+    return lastWinner;
+  }
 }
